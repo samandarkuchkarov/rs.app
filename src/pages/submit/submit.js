8209d9051a8ce6d84fabbf6ca0d1a33e8b02a226
samandarkuchkarov <samandar8939522@gmail.com>
2021-02-06 22:49:59 +0500

    da

--- src/pages/submit/submit.js
+++ src/pages/submit/submit.js
@@ -2,18 +2,21 @@ import Select from '@material-ui/core/Select';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
-import React from 'react';
+import React,{useEffect} from 'react';
 import './submit.css'
 import {useDispatch,useSelector} from 'react-redux'
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-import {updatedPost} from './../../actions/posts'
+import {updatedPost,getPosts} from './../../actions/posts'
 export const Submitask = ()=>{
 
     const [label, setLabel] = React.useState('');
     const [link, setLink] = React.useState()
     const dispatch = useDispatch()
     let unuqiue = 3657345;
+    useEffect(()=>{
+		dispatch(getPosts())
+	},[dispatch])
     const changeHandler = (event)=>{
         setLink({ ...link, text: event.target.value })
     }
@@ -24,11 +27,11 @@ export const Submitask = ()=>{
     const posts = useSelector((state)=>state.posts)
     const submit = ()=>{
         posts.forEach((user)=>{
-            if(user.email===localStorage.getItem('email')){
+            if(user.email===localStorage.getItem('email')&&user.length!==0){
               
                 user.tasks.forEach((item,i)=>{
                    
-                   if(item[0]===label){
+                   if(item[0]===label&&item.length!==0){
                     user.tasks[i][2]=link.text
 
                     dispatch(updatedPost(user._id,user))
@@ -41,12 +44,20 @@ export const Submitask = ()=>{
     }
     let list = []
     posts.forEach((user)=>{
-        if(user.email===localStorage.getItem('email')){
+        if(user.email===localStorage.getItem('email')&& user.length!==0){
             list=user.tasks.filter(item=>{
                 
                 return item[2]!==undefined
             })
         }})
+        let z =[]
+        if(posts.length!==0){
+           z = posts[0].tasks.map((item)=>{
+                return (
+                    <MenuItem key = {unuqiue++}value={item[0]}>{item[0]}</MenuItem> 
+                )})
+        
+        }
 let number = 1
     return(
         <>
@@ -67,11 +78,7 @@ let number = 1
           value={label}
           onChange={handleChange}
         >
-         { posts[0].tasks.map((item)=>{
-        return (
-            <MenuItem key = {unuqiue++}value={item[0]}>{item[0]}</MenuItem> 
-        )
-    })}
+       {z}
         </Select>
       </FormControl>
        <form  noValidate autoComplete="off" onChange={ changeHandler }>
